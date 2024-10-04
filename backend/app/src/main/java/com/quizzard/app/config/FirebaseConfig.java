431846cd4c9d8a035fc19bc9e7816c7da26249c3
Stefan-ys <stefanstoyanov001@gmail.com>
2024-10-04 14:32:31 +0300

    fix: fix firebase config

--- backend/app/src/main/java/com/quizzard/app/config/FirebaseConfig.java
+++ backend/app/src/main/java/com/quizzard/app/config/FirebaseConfig.java
@@ -3,39 +3,27 @@ package com.quizzard.app.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${DB_FIREBASE_PROJECT_ID}")
-    private String projectId;
-
-    @Value("${DB_FIREBASE_PRIVATE_KEY}")
-    private String privateKey;
-
-    @Value("${DB_FIREBASE_CLIENT_EMAIL}")
-    private String clientEmail;
-
-    @Value("${DB_FIREBASE_STORAGE_BUCKET}")
-    private String storageBucket;
-
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-admin-sdk.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(privateKey.getBytes())))
-                .setProjectId(projectId)
-                .setStorageBucket(storageBucket)
-                .setServiceAccountId(clientEmail)
-                .build();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        return FirebaseApp.initializeApp(options);
+            return FirebaseApp.initializeApp(options);
+        }
+        return FirebaseApp.getInstance();
     }
 }
