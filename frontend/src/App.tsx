2739a4c237686d3ce4a4f19e442c050b8e343913
Stefan-ys <stefanstoyanov001@gmail.com>
2024-10-05 21:15:19 +0300

    feat: frontend jwt token handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import RegisterPage from './pages/Register';
 import LoginPage from './pages/Login';
 import UsersPage from './pages/Users';
@@ -7,22 +9,25 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import './App.css';
 import { AuthProvider } from './context/AuthProvider';
+import { themeOptions } from './themes/themeOptions';
 
-
+const theme = createTheme(themeOptions);
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Router>
-      <Navbar /> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </AuthProvider>
   );
 };
--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,37 +1,54 @@
 import { createContext, useState, ReactNode } from "react";
 import { login, logout, register } from '../services/authServices';
-
+import  * as jwt_decode from 'jwt-decode';
+import axios from 'axios';
 
 interface AuthContextType {
-    user: string | null;
+    user: User | null;
     login: (username: string, password: string) => Promise<void>;
     register: (username: string, email: string, password: string, confirmPassword: string) => Promise<void>;
     logout: () => Promise<void>;
 }
 
+interface User {
+    username: string;
+}
+
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [user, setUser] = useState<string | null>(null);
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [user, setUser] = useState<User | null>(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decodedToken: any = jwt_decode(token);
+            return { username: decodedToken.sub };    
+        }
+        return null;
+    });
 
     const handleLogin = async (username: string, password: string) => {
-        const data = await login(username, password);
-        setUser(data.user);
+        const response = await login(username, password);
+        const { token, user } = response;
+        localStorage.setItem('token', token);
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        setUser(user);
     };
 
     const handleRegister = async (username: string, email: string, password: string, confirmPassword: string) => {
-        const data = await register(username, email, password, confirmPassword);
-        setUser(data.user);
+        const response = await register(username, email, password, confirmPassword);
+        handleLogin(username, password);
     };
 
     const handleLogout = async () => {
         await logout();
+        localStorage.removeItem('token');
+        delete axios.defaults.headers.common['Authorization'];
         setUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{user, login: handleLogin, register: handleRegister, logout: handleLogout}}>
+        <AuthContext.Provider value={{ user, login: handleLogin, register: handleRegister, logout: handleLogout }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
